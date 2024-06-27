9a5c503edb9f2257767c2cb031c4702d987d477b
Jack Newcombe <jack@newcombe.io>
2024-06-27 19:38:24 +0100

    lint

--- Formula/Simian.rb
+++ Formula/Simian.rb
@@ -3,9 +3,8 @@ class Simian < Formula
   homepage "https://github.com/jnewc/Simian"
   url "https://github.com/jnewc/Simian"
   license "MIT"
-
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "0.1.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   def install
     bin.install ".build/release/simian"

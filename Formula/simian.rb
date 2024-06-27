80c8db19f684fc7b6669b96d9e277bfbd4377ae0
Jack Newcombe <jack@newcombe.io>
2024-06-27 22:51:44 +0100

    rename

--- Formula/simian.rb
+++ Formula/simian.rb
@@ -1,9 +1,7 @@
 class Simian < Formula
-  name "simian"
   desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
-  url "https://github.com/jnewc/Simian"
-  version "0.1.0"
+  url "https://github.com/jnewc/Simian/releases/download/0.1.0/simian-release-0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 

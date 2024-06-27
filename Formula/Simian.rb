80901cab1c8a32443b1abbdc1cfc67a686661089
Jack Newcombe <jack@newcombe.io>
2024-06-27 19:32:29 +0100

    lint

--- Formula/Simian.rb
+++ Formula/Simian.rb
@@ -1,5 +1,5 @@
 class Simian < Formula
-  desc "A tool for managing iOS simulators"
+  desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
   url "https://github.com/jnewc/Simian"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
@@ -10,5 +10,6 @@ class Simian < Formula
   end
 
   test do
+    puts "..."
   end
 end

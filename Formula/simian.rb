d8a5d2e04bab942e3de3d3c796cdd5bb281f39d4
Jack Newcombe <jack@newcombe.io>
2024-06-27 22:56:16 +0100

    correct sha 256

--- Formula/simian.rb
+++ Formula/simian.rb
@@ -2,7 +2,7 @@ class Simian < Formula
   desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
   url "https://github.com/jnewc/Simian/releases/download/0.1.0/simian-release-0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f78a8264c9326fb2f891a96f5e0df9fc07980d3a823c753de312a0babdeb5da8"
   license "MIT"
 
   def install

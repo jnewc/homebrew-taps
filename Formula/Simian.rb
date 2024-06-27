4f301a572e5e90401dc8844f64514f45c45d8408
Jack Newcombe <jack@newcombe.io>
2024-06-27 19:35:09 +0100

    Add version attr

--- Formula/Simian.rb
+++ Formula/Simian.rb
@@ -2,8 +2,10 @@ class Simian < Formula
   desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
   url "https://github.com/jnewc/Simian"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
+
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.1.0"
 
   def install
     bin.install ".build/release/simian"

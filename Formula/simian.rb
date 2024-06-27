4fe2a25c18b7465732f96d2d35bce859a91b7187
Jack Newcombe <jack@newcombe.io>
2024-06-27 23:25:02 +0100

    update hash

--- Formula/simian.rb
+++ Formula/simian.rb
@@ -2,7 +2,7 @@ class Simian < Formula
   desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
   url "https://github.com/jnewc/Simian/releases/download/0.1.0/simian-release-0.1.0.tar.gz"
-  sha256 "f78a8264c9326fb2f891a96f5e0df9fc07980d3a823c753de312a0babdeb5da8"
+  sha256 "eb699dcbd6bf32964280dd3aec140a543dd32418fa5552ded0367e9d9fe3e8a8"
   license "MIT"
 
   def install

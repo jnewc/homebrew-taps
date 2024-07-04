8c9ff8575e1af9c4686e0de620928b56eca81c78
jnewc <jnewc@users.noreply.github.com>
2024-07-04 20:18:16 +0000

    Apply automatic changes

--- Formula/simian.rb
+++ Formula/simian.rb
@@ -1,8 +1,8 @@
 class Simian < Formula
   desc "Tool for managing iOS simulators"
   homepage "https://github.com/jnewc/Simian"
-  url "https://github.com/jnewc/Simian/releases/download/0.1.0/simian-release-0.1.0.tar.gz"
-  sha256 "eb699dcbd6bf32964280dd3aec140a543dd32418fa5552ded0367e9d9fe3e8a8"
+  url "https://github.com/jnewc/Simian/releases/download/0.2.0/simian-release-0.2.0.tar.gz"
+  sha256 "49966f0e6b1448c05e5f47fc69af67719afeaa9a5321e7d7c9fa7e98e8035672"
   license "MIT"
 
   def install
